6e7e176513a16d5ef00872eb398f387e576ee273
Purnendu Shukla <pshukla_be15@thapar.edu>
2019-05-30 18:40:34 +0530

    connected to firebase databse

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,15 +1,25 @@
-import { Component, Output, EventEmitter } from '@angular/core'
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from '../shared/data-storage.service';
 
 @Component({
-	selector : "app-header",
-	templateUrl : "header.component.html"
+  selector: 'app-header',
+  templateUrl: './header.component.html'
 })
-export class HeaderComponent{
+export class HeaderComponent {
+  constructor(private dataStorageService: DataStorageService) {}
 
-	// @Output() selectionEmitter = new EventEmitter<string>();
-  //
-	// onSelect(option: string){
-	// 	this.selectionEmitter.emit(option);
-	// }
+  onSaveData() {
+    this.dataStorageService.storeRecipes()
+      .subscribe(
+        (response: Response) => {
+          console.log(response);
+        }
+      );
+  }
 
+  onFetchData() {
+    this.dataStorageService.getRecipes();
+  }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -56,4 +56,9 @@ export class RecipeService {
     this.recipeChanged.next(this.recipes.slice());
   }
 
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipeChanged.next(this.recipes.slice());
+  }
+
 }
